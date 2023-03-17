38df39f190cc6050c88e1a1d740e2339b599d647
Mohamed Ibrahem <mh18399@gmail.com>
2023-03-18 01:57:01 +0200

    bul_post drugs
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- medicines/views.py
+++ medicines/views.py
@@ -1,3 +1,41 @@
+
+from django.db.models.aggregates import Count
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action, permission_classes, api_view
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny, \
+    DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from .serializers import *
+from medicines.pagination import DefaultPagination
 from django.shortcuts import render
 
-# Create your views here.
+
+class MedicineViewSet(ModelViewSet):
+    queryset = Medicine.objects.all()
+    serializer_class = MedicineSerializer
+    pagination_class = DefaultPagination
+
+    @action(detail=False, methods=['POST'])
+    def bulk_create(self, request):
+        serializer = MedicineSerializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        medicines = serializer.save()
+        return Response(MedicineSerializer(medicines, many=True).data)
+
+
+class DrugViewSet(ModelViewSet):
+    queryset = Drug.objects.all()
+    serializer_class = DrugSerializer
+    pagination_class = DefaultPagination
+
+    @action(detail=False, methods=['POST'])
+    def bulk_create(self, request):
+        serializer = DrugSerializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        drugs = serializer.save()
+        return Response(DrugSerializer(drugs, many=True).data)
