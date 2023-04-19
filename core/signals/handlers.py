bef1ce466ecff8426258ad6d6896b6754623ecc6
Mo-Adel1 <102308283+Mo-Adel1@users.noreply.github.com>
2023-04-19 03:18:38 +0200

    created signals to create shoping cart for every patient

--- core/signals/handlers.py
+++ core/signals/handlers.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from core.models import *
-
+from store.models import Cart
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_Profile_for_new_user(sender, **kwargs):
@@ -11,7 +11,8 @@ def create_Profile_for_new_user(sender, **kwargs):
       print(kwargs['instance'])
       print(kwargs['instance'].profile_type)
       if kwargs['instance'].profile_type == 'PAT':
-        Patient.objects.create(user=kwargs['instance'])
+        patient = Patient.objects.create(user=kwargs['instance'])
+        Cart.objects.create(customer=patient)
       elif kwargs['instance'].profile_type == 'DR':
         Doctor.objects.create(user=kwargs['instance'])
 
