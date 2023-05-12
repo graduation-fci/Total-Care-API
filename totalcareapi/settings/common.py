2e25b741e72d799edc511f2cae76524b1e707bb4
Mohamed Ibrahem <mh18399@gmail.com>
2023-05-12 17:27:37 +0300

    updated allowed orgins
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- totalcareapi/settings/common.py
+++ totalcareapi/settings/common.py
@@ -71,7 +71,9 @@ INTERNAL_IPS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
+    'http://localhost:8000',
     'http://127.0.0.1:8001',
+    'http://127.0.0.1:8000',
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://localhost:3000',
